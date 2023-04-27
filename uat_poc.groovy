1e2f34c095690ce664f568962fd672cd74bda9e5
Roalvies <107957422+Roalvies@users.noreply.github.com>
2023-04-26 22:43:34 -0400

    Update uat_poc.groovy

--- uat_poc.groovy
+++ uat_poc.groovy
@@ -1,34 +1,21 @@
-pipelineJob('Job-poc') {
+pipelineJob('uat-poc') {
     definition {
         cps {
-            script(readFileFromWorkspace('uat.jenkinsfile'))
+            script(readFileFromWorkspace('aut.jenkinsfile'))
             sandbox()
         }
     }
-    
-    parameters {
+
+    parameters{
         activeChoiseParam('DESTINO'){
             choiceType('SINGLE_SELECT')
-            groovyScripts {
-                script("['q1','q2','q3','q4','q5']")
-            
-            fallbackScript('return ["ERROR"]')
-            
-            description('Seleccion el Destino del proyecto. (Campo obligatorio)')
+            groovyScripts{
+                script(['Asp','Clou_B','Cloud_C'])
+
+                fallbackScript('return ["ERROR"]')
+
+                description('Selecciona la maquina para desplegar el proyecto')
             }
-        }
-    }
-
-    parameters {
-        stringParam('Tipo_Perfil', '' , 'Favor Ingrese su Perfil')
-        booleanParam('cargarproperties', false, 'Indicar si desea cargar archivo de propiedades.')
-    }
-
-    parameters {
-        gitParam('branch') {
-            description 'Favor seleccione branch'
-            type 'BRANCH'
-            default 'testing'
         }
     }
 
